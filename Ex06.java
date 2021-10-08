1913510ddbea9ec82b64b2b96c03f02d5a48b7b7
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 14:10:53 -0300

    Create Ex06.java

---
+++ Ex06.java
@@ -0,0 +1,12 @@
+package com.codegym.task.task01.task0106;
+
+/* 
+Bugs and features
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("It's not a bug - it's a feature.");
+    }
+}
