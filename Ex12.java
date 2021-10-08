e2b06957b35ecc44b49aa49c787bf10447f7989c
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 14:32:33 -0300

    Create Ex12.java

---
+++ Ex12.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task01.task0114;
+
+/* 
+CodeGym. Learn once - use anywhere
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String message = "CodeGym. Learn once - use anywhere";
+
+       for(int i = 0; i < 10 ; i++) {
+            System.out.println(message);
+        }
+    }
+}
