1981e54ceed1e4a254227716ea276aab37a48348
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 14:07:04 -0300

    Create Ex04.java

---
+++ Ex04.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task01.task0103;
+
+/* 
+My young friend
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int aYear = 3126;
+        int yearB = aYear - 8;
+        
+        System.out.println(yearB);
+        
+    }
+}
