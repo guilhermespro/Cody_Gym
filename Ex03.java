7b2a42d0b002ba4af8d6eacb42d2a858548e04c9
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 13:28:48 -0300

    Create Ex03.java

---
+++ Ex03.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task01.task0102;
+
+/* 
+Kind words for the teacher
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        for(int i = 0. i < 5 ; i++) {
+            System.out.println("Ellie is clever");
+        }
+    }
+}
