904640ed1b629d37298f9eee7192e11e79bf8e18
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 14:26:54 -0300

    Create Ex10.java

---
+++ Ex10.java
@@ -0,0 +1,14 @@
+package com.codegym.task.task01.task0111;
+
+/* 
+One variable is not enough
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        String s = "Hello";
+    }
+}
