ab1dfc1fc1177ecebfde023f0061f8a2f4e34ca3
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 13:30:00 -0300

    Update Ex03.java

--- Ex03.java
+++ Ex03.java
@@ -7,7 +7,7 @@ Kind words for the teacher
 
 public class Solution {
     public static void main(String[] args) {
-        for(int i = 0. i < 5 ; i++) {
+        for(int i = 0; i < 5 ; i++) {
             System.out.println("Ellie is clever");
         }
     }
