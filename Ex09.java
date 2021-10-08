24f9f028dea476161a0da2c87ea79a1ef16929de
guilhermespro <guilherme.spro@gmail.com>
2021-10-08 14:25:26 -0300

    Create Ex09.java

---
+++ Ex09.java
@@ -0,0 +1,13 @@
+package com.codegym.task.task01.task0110;
+
+/* 
+A variable on our screen
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String name = "Bob";
+        System.out.println(name);
+    }
+}
